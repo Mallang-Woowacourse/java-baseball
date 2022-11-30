c9b4e6b0adbb7786ea57639e6e232ef56b42147d
shinD <huipulci1@naver.com>
2022-11-30 15:54:03 +0900

    feat: 애플리케이션 실행 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.application.BaseBallGameApplication;
+import baseball.application.BaseBallGameController;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        BaseBallGameController controller = new BaseBallGameController(inputView, outputView);
+        new BaseBallGameApplication(controller).start();
     }
 }
