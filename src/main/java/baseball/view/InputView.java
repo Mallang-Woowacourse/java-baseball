faebab9cbb7e2230c5518c611966129073470e8b
shinD <huipulci1@naver.com>
2022-11-30 15:53:10 +0900

    feat: view 관련 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,29 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String INPUT_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public String inputNumber() {
+        System.out.print(INPUT_NUMBER);
+        String input = Console.readLine();
+        validateNumber(input);
+        return input;
+    }
+
+    private void validateNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String inputRestart() {
+        System.out.println(INPUT_RESTART);
+        return Console.readLine();
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,13 +2,39 @@ package baseball.view;
 
 import baseball.domain.compare.CompareResult;
 
+import static java.lang.String.format;
+
 public class OutputView {
+
+    private static final String START ="숫자 야구 게임을 시작합니다.";
+    private static final String STRIKE_FORMAT ="%d스트라이크";
+    private static final String BALL_FORMAT ="%d볼";
+    private static final String NOTHING ="낫싱";
+    private static final String RESULT_FORMAT = BALL_FORMAT + " " + STRIKE_FORMAT;
+    private static final String ANSWER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     public void printStart() {
+        System.out.println(START);
     }
 
     public void printCorrectAnswer() {
+        System.out.println(ANSWER);
     }
 
     public void printCompareResult(CompareResult result) {
+        System.out.println(parseResultMessage(result));
+    }
+
+    private String parseResultMessage(CompareResult result) {
+        if (result.isNothing()) {
+            return NOTHING;
+        }
+        if (result.getBall() == 0) {
+           return format(STRIKE_FORMAT, result.getStrike());
+        }
+        if (result.getStrike() == 0) {
+          return format(BALL_FORMAT, result.getBall());
+        }
+        return format(RESULT_FORMAT, result.getBall(), result.getStrike());
     }
 }
